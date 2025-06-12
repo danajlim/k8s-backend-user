19d90e5f50460b06b637015ae68db406eb628b77
danajlim <danalim0819@gmail.com>
2025-06-12 23:55:57 +0900

    MySQL 연동

---
+++ src/main/java/com/welab/k8s_backend_user/domain/SiteUser.java
@@ -0,0 +1,30 @@
+package com.welab.k8s_backend_user.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Entity
+@Table(name = "site_user")
+@Getter @Setter
+public class SiteUser {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", unique = true, nullable = false)
+    private String userId;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "deleted", nullable = false)
+    private Boolean deleted = false;
+}
